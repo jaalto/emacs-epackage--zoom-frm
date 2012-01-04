2b652638f0572c23fa652da114b589770c0baa51
Jari Aalto <jari.aalto@cante.net>
2012-01-04 17:19:40 +0200

    *-pkg-0loaddefs: Fix paths
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- epackage/zoom-frm-epkg-0loaddefs.el
+++ epackage/zoom-frm-epkg-0loaddefs.el
@@ -1,10 +1,10 @@
 
 ;;;### (autoloads (toggle-zoom-frame zoom-frm-unzoom zoom-frm-out
-;;;;;;  zoom-frm-in frame-zoom-font-difference zoom) "zoom-frm" "../zoom-frm.el"
-;;;;;;  (20149 43092))
-;;; Generated autoloads from ../zoom-frm.el
+;;;;;;  zoom-frm-in frame-zoom-font-difference zoom) "../zoom-frm"
+;;;;;;  "../zoom-frm.el" (20149 43092))
+;;; Generated autoloads from zoom-frm.el
 
-(let ((loads (get 'zoom 'custom-loads))) (if (member '"zoom-frm" loads) nil (put 'zoom 'custom-loads (cons '"zoom-frm" loads))))
+(let ((loads (get 'zoom 'custom-loads))) (if (member '"../zoom-frm" loads) nil (put 'zoom 'custom-loads (cons '"../zoom-frm" loads))))
 
 (defvar frame-zoom-font-difference 1 "\
 *Number of points to change the frame font size when zooming
@@ -12,7 +12,7 @@ using commands `zoom-frm-in' and `zoom-frm-out'.
 The absolute value of this must be less than the current font size,
 since the new font size cannot be less than 1 point.")
 
-(custom-autoload 'frame-zoom-font-difference "zoom-frm" t)
+(custom-autoload 'frame-zoom-font-difference "../zoom-frm" t)
 
 (autoload 'zoom-frm-in "zoom-frm" "\
 Zoom FRAME in by `frame-zoom-font-difference', making text larger.
@@ -43,4 +43,3 @@ Alternately zoom/unzoom FRAME by `frame-zoom-font-difference'.
 \(fn &optional FRAME)" t nil)
 
 ;;;***
-(provide 'zoom-frm-epkg-0loaddefs)
